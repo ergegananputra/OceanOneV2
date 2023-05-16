d44fd13ead597d4a3eff278c24e5d964983fb725
ergegananputra <ergegananputra@gmail.com>
2023-05-16 10:58:04 +0700

    update

---
+++ tweak/javascript/tweak.js
@@ -0,0 +1,11 @@
+function takeActionButton(){
+    alert("I am taking action");
+}
+
+function defaultAlign(){
+    var editTextArea = document.getElementById("editableTextArea").innerHTML;
+
+    
+
+
+}
